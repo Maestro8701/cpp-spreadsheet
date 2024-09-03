41ef881ea83610d528bd61621c16425754491644
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2024-09-03 20:28:36 +0700

    Update cell.h

--- spreadsheet/cell.h
+++ spreadsheet/cell.h
@@ -5,6 +5,7 @@
 
 #include <functional>
 #include <unordered_set>
+#include <optional>    
 
 class Sheet;
 
@@ -16,21 +17,23 @@ public:
     void Set(std::string text);
     void Clear();
 
-    Value GetValue() const override;
+    Value GetValue() const override; 
     std::string GetText() const override;
     std::vector<Position> GetReferencedCells() const override;
-
-    bool IsReferenced() const;
-
+    
 private:
     class Impl;
     class EmptyImpl;
     class TextImpl;
     class FormulaImpl;
 
+    bool HasCircularDependency(const Impl& impl) const;
+    void RefCellsProcessing(const std::vector<Position>& vec);
+    void InvalidateAllCache();
+
     std::unique_ptr<Impl> impl_;
-
-    // Добавьте поля и методы для связи с таблицей, проверки циклических 
-    // зависимостей, графа зависимостей и т. д.
-
+    Sheet& sheet_;
+    std::unordered_set<Cell*> dep_cells_; // зависимые ячейки
+    std::unordered_set<Cell*> ref_cells_; // используемые ячейки
+    mutable std::optional<Value> cache_;
 };
