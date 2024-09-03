fdbbb701c1bc553ffd0526e0a4766017c2d1e3e4
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2024-09-03 20:28:54 +0700

    Update sheet.h

--- spreadsheet/sheet.h
+++ spreadsheet/sheet.h
@@ -1,1 +1,38 @@
-/* разместите здесь свой код */
+#pragma once
+
+#include "cell.h"
+#include "common.h"
+
+#include <functional>
+#include <unordered_map>
+
+class CellHasher {
+public:
+    size_t operator()(const Position p) const {
+        return std::hash<std::string>()(p.ToString());
+    }
+};
+
+class Sheet : public SheetInterface {
+public:
+    ~Sheet();
+
+    void SetCell(Position pos, std::string text) override;
+
+    const CellInterface* GetCell(Position pos) const override;
+    CellInterface* GetCell(Position pos) override;
+
+    void ClearCell(Position pos) override;
+
+    Size GetPrintableSize() const override;
+
+    void PrintValues(std::ostream& output) const override;
+    void PrintTexts(std::ostream& output) const override;
+    
+    const Cell* GetPtr(Position pos) const;
+    Cell* GetPtr(Position pos);
+
+private:
+	std::unordered_map<Position, std::unique_ptr<Cell>, CellHasher> cells_;
+    void IsPosValid(const Position& pos) const;
+};
